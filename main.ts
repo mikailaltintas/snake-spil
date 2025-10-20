04bd83a6439ec6c8398307a71285eb405f897f82
mikailaltintas <mikailaltintas7@gmail.com>
2025-10-20 12:11:48 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
 tiles.setCurrentTilemap(tilemap`level1`)
+game.onUpdate(function () {
+    let mySprite: Sprite = null
+    controller.moveSprite(mySprite)
+    scene.cameraFollowSprite(mySprite)
+})
